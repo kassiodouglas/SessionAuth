7e33661e9e13463667942ff2f2b0ce09feea8450
cassio.souza <cassio.souza@grupotel.com.br>
2021-12-20 14:58:21 -0300

    up Sessionauth.js

--- src/config/SessionAuth.php
+++ src/config/SessionAuth.php
@@ -4,5 +4,5 @@ return [
 
     'dominio' => env('APP_DOMINIO', 'http://localhost'),
     'id_system' => env('SESSION_ID_SYSTEM', null),
-    'session_path' => env('SESSION_PATH', null)
+    'session_path' => env('SESSION_PATH', null)    
 ];
